dbf9f19b2099bba7ef30d5fb4691a1dd1e3dfba4
root <root@localhost.localdomain>
2015-10-30 13:00:22 +0800

    Few more comments added

--- reboot_range.sh
+++ reboot_range.sh
@@ -1,4 +1,31 @@
 #! /bin/bash
+#
+# Script to reboot a set of network switches located at addresses
+# 'begin', 'begin'+1, ... , 'end' (i.e. arithm. sequence).
+#
+# Values 'begin' and 'end' are read from STDIN, alternatively, 
+# uncommenting 'INSTANCE' string (see below) makes these values
+# hard-coded (as in initial e-mail).
+#
+#
+# 
+# (C) Copyright 2015, TP-Link Inc, konstantin.mauch@tp-link.com
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307 USA
+
 
 # Time (seconds) to wait for remote switch to respond before recognizing it as non-responding. INTEGER
 sec2wait=2
