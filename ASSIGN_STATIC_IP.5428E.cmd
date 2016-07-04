f8fb8ec4e302001244d508a604526018f2a9080c
root <you@example.com>
2016-07-04 13:02:09 +0300

    Nasty files removed

--- ASSIGN_STATIC_IP.5428E.cmd
+++ ASSIGN_STATIC_IP.5428E.cmd
@@ -1,7 +1,7 @@
 echo "1. Assigining static IP settings"
-./inject.sh 192.168.0.38 static 5428E 192.168.0.40 255.255.255.0 >./_static.log
+./inject.sh 192.168.1.1 static 5428E 192.168.1.2 255.255.255.0 #>./_static.log
 echo "1. IP address settings issued"
 
 echo "2. Saving changed IP adddress settings. (Supressing STDOUT output is obvious.)"
-./inject.sh 192.168.0.40 save 5428E >./_save.log
+./inject.sh 192.168.1.2 save 5428E >./_save.log
 echo "2. Saved"
